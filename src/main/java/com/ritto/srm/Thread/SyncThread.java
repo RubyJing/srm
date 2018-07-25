955a34743ec84aa909ec7e542422da7a127c2de1
Eiden J.P Zhou <303713060@qq.com>
2018-07-25 17:59:43 +0800

    --代码健壮性增强 --优化了同步线程 --添加了多线程同时同步管理 --UI添加了重复点击提醒和同步启动提醒 --同步失败时保留当前同步进度，下次可以从终止的地方继续开始

--- src/main/java/com/ritto/srm/Thread/SyncThread.java
+++ src/main/java/com/ritto/srm/Thread/SyncThread.java
@@ -5,8 +5,10 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
 import javax.transaction.Transactional;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @Auther: Eiden J.P Zhou
@@ -15,7 +17,7 @@ import java.util.List;
  * @Modified By:
  */
 public class SyncThread extends Thread{
-    public static Integer jdt = 0 ;
+    public static Map<String,Integer> jdt = new HashMap<>();
     protected JdbcTemplate jdbcTemplate1;
 
     protected JdbcTemplate jdbcTemplate2;
@@ -23,22 +25,25 @@ public class SyncThread extends Thread{
     EntityManager entityManager;
 
     private String tabname;
-    public SyncThread(String name,JdbcTemplate jt1,JdbcTemplate jt2,EntityManager em){
+    private int dataindex;
+    public SyncThread(String name,int index,JdbcTemplate jt1,JdbcTemplate jt2,EntityManager em){
         tabname = name;
+        dataindex = index;
         jdbcTemplate1 = jt1;
         jdbcTemplate2 = jt2;
         entityManager = em;
-        jdt = 0;
+        jdt.put(tabname,0);
     }
 
     @Transactional
     @Override
     public void run(){
         int count = Integer.parseInt(entityManager.createNativeQuery("SELECT COUNT(*) FROM "+tabname).getSingleResult().toString());
-         for (int i=0;i<count;i+=10){
+         for (int i=dataindex;i<count;i+=10){
             Query query = entityManager.createNativeQuery("SELECT * FROM "+tabname+" WHERE 1=1 LIMIT "+i+",10");
 //			query.setParameter(1,tablename);
             List objectList = query.getResultList();
+            int k = 0;
             Iterator it = objectList.iterator();
             while(it.hasNext()){
                 Object[] object = (Object[])it.next();
@@ -63,16 +68,20 @@ public class SyncThread extends Thread{
                 sql.append(" )");
                 if(jdbcTemplate2.update(sql.toString())>0){
                     System.out.println("seccess");
+                    k++;
+                    jdbcTemplate1.update("UPDATE sync SET data_index = "+ (i+k+1) + " WHERE sync_tab_name = '" + tabname +"'");
                 }
             }
-            jdt = i*100/count>100?100:i*100/count;
+            jdt.put(tabname,i*100/count>100?100:i*100/count);
         }
-        jdt = 100;
+        jdt.put(tabname,100);
          //同步完成后
         StringBuffer sql = new StringBuffer();
         sql.append("UPDATE sync SET last_sync_state ='已同步' WHERE sync_tab_name = '");
         sql.append(tabname);
         sql.append("'");
+        sql.append(" AND data_index = ");
+        sql.append(count);
         if (jdbcTemplate1.update(sql.toString())>0){
             System.out.println("同步成功");
         }
